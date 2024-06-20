522fdb0a053cffc8ce8a159867a20f2a640dae86
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-06-20 13:47:44 +0800

    feat: powershell configs

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,7 @@
+oh-my-posh init pwsh --config "C:\Users\Users\Documents\PowerShell\tokyonight_storm.omp.json" | Invoke-Expression
+
+if ($env:TERM_PROGRAM -eq "nvim") { . "$(nvim--locate-shell-integration-path pwsh)" }
+
+$Env:KOMOREBI_CONFIG_HOME = 'C:\Users\Users\.config\komorebi'
+
+Import-Module -Name Terminal-Icons
