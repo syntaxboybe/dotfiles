f404122c07dd55a49cf74989ca2f121a51500a48
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-07-17 15:28:58 +0800

    feat: zoxide & btop

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,7 +1,11 @@
 oh-my-posh init pwsh --config "C:\Users\Users\Documents\PowerShell\tokyonight_storm.omp.json" | Invoke-Expression
 
-if ($env:TERM_PROGRAM -eq "nvim") { . "$(nvim--locate-shell-integration-path pwsh)" }
+if ($env:TERM_PROGRAM -eq "nvim")
+{ . "$(nvim--locate-shell-integration-path pwsh)" 
+}
 
 $Env:KOMOREBI_CONFIG_HOME = 'C:\Users\Users\.config\komorebi'
 
 Import-Module -Name Terminal-Icons
+
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
