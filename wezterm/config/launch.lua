482a46436381ed238e1cd506bc88ac43bc658b39
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-07-18 15:00:36 +0800

    feat: wezterm config

---
+++ wezterm/config/launch.lua
@@ -0,0 +1,37 @@
+local platform = require('utils.platform')()
+
+local options = {
+   default_prog = {},
+   launch_menu = {},
+}
+
+if platform.is_win then -- Windows
+   options.default_prog = { 'pwsh' }
+   options.launch_menu = {
+      { label = 'PowerShell Core', args = { 'pwsh' } },
+      { label = 'PowerShell Desktop', args = { 'powershell' } },
+      { label = 'Command Prompt', args = { 'cmd' } },
+      { label = 'Nushell', args = { 'nu' } },
+      {
+         label = 'Git Bash',
+         args = { 'C:\\Program Files\\Git' },
+      },
+   }
+elseif platform.is_mac then -- macOS
+   options.default_prog = { '/opt/homebrew/bin/fish', '-l' }
+   options.launch_menu = {
+      { label = 'Bash', args = { 'bash', '-l' } },
+      { label = 'Fish', args = { '/opt/homebrew/bin/fish', '-l' } },
+      { label = 'Nushell', args = { '/opt/homebrew/bin/nu', '-l' } },
+      { label = 'Zsh', args = { 'zsh', '-l' } },
+   }
+elseif platform.is_linux then -- Linux
+   options.default_prog = { 'fish', '-l' }
+   options.launch_menu = {
+      { label = 'Bash', args = { 'bash', '-l' } },
+      { label = 'Fish', args = { 'fish', '-l' } },
+      { label = 'Zsh', args = { 'zsh', '-l' } },
+   }
+end
+
+return options
