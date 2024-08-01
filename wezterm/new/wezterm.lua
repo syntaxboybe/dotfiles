c27fda693f10b2779634eb69086a3ef2e136fbf5
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-08-02 01:26:18 +0800

    update: colorscheme

--- wezterm/new/wezterm.lua
+++ wezterm/new/wezterm.lua
@@ -7,19 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 config.default_prog = { "pwsh.exe", "-NoLogo" }
 -- my coolnight colorscheme
-config.colors = {
-	foreground = "#CBE0F0",
-	background = "#011423",
-	cursor_bg = "#00FFFF",
-	cursor_border = "#00FFFF",
-	cursor_fg = "#011423",
-	selection_bg = "#033259",
-	selection_fg = "#CBE0F0",
-	ansi = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#0FC5ED", "#a277ff", "#24EAF7", "#24EAF7" },
-	brights = { "#214969", "#E52E2E", "#44FFB1", "#FFE073", "#A277FF", "#a277ff", "#24EAF7", "#24EAF7" },
-}
+config.color_scheme = "Catppuccin Mocha"
 
-config.font = wezterm.font("JetBrainsMono Nerd Font")
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Bold" })
 config.font_size = 10
 
 config.enable_tab_bar = false
