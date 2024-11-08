8e804cea448ca5e9a681b2734abef1dbfaa0a567
Syntaxboybe <beboymegalbioeyana@gmail.com>
2024-11-08 21:00:53 +0800

    feat: shell_script
    added .sh file for scripting

---
+++ shell_script/apps.sh
@@ -0,0 +1,8 @@
+#apps.sh
+clear
+echo "Checking versions of installed apps..."
+ls
+yasbc --version
+komorebic --version
+echo "Done!"
+# cat apps.sh
---
+++ shell_script/script1.sh
@@ -0,0 +1,11 @@
+#script1.sh
+clear
+echo "Starting shell script.."
+ls
+python --version
+node -v
+npm -v
+git --version
+composer -V
+echo "Ending shell script.."
+# cat script1.sh
